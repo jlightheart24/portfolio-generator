d94cb5e8511b704466bf334c2e5f4a7d1ad79b63
Jonny Lightheart <jonnylightheart@gmail.com>
2020-09-21 17:57:39 -0600

    installed inquirer

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./page-template.js');
+const inquirer = require('inquirer');
+const { AsyncLocalStorage } = require('async_hooks');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./page-template.js');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    console.log('Portfolio complete! Checkout the index.html to see the output!')
-});
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Checkout the index.html to see the output!')
+// });
